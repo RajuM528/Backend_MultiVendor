5214022e76cf3c4cb8a0ff0e571d847249f2f7af
RajuM528 <rajmenda056@gmail.com>
2024-12-28 15:34:44 +0530

    made changes for PORT and URL with homepage in index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const produtRoutes = require('./routes/productRoutes');
 const path = require('path');
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; // here we used process.env , when we deployed the app then if we try to acces the app via a link from the deployement then it will open in the port other wise we will get an erro like cannot GET/
 dotEnv.config();  // to access the values in .env file 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -28,6 +28,6 @@ app.listen(PORT, () => {
     console.log(`Servere started and running at ${PORT}`);
 })
 
-app.use('/home', (req, res) => {
-    res.send("<h1> Welcome to SUBY")
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to Raju")
 });
